86ec9e1dea2e65a3156a127aec1d6f4044894b76
timmy <timmy@timmy-Lenovo-G50-80>
2016-03-30 11:45:03 +0200

    Bug solved. Moved the output to a file. Therefore the output is read from the file.
    Increased execution time.

--- src/core/Reader.java
+++ src/core/Reader.java
@@ -1,8 +1,10 @@
 package core;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Javadoc dani
@@ -11,49 +13,76 @@ import java.io.InputStreamReader;
  */
 public class Reader {
 
+	private final static String output = "output";
+	private final static String error = "error";
 	private String secondsOfGeneration;
     private String jarName;
     private Process dataGenerator;
-    
+    private File outputFile;
+    private File errorFile;
     private BufferedReader brStd;
     private BufferedReader brErr;
-    private InputStreamReader isr;
     
 	public Reader(Integer seconds, String pathToGenerator){
 		this.secondsOfGeneration = seconds.toString();
 		this.jarName = pathToGenerator;
-		//Executing the data generator
+		
+		//Create temporary file to save outputs
+		try {
+			outputFile = File.createTempFile( output, "txt");
+			errorFile = File.createTempFile( error, "txt");
+			System.out.println( "Output file: " + outputFile.getAbsolutePath() );
+		} catch (IOException e1) {
+			System.err.println("Cannot create temporary Files\nTerminate execution");
+			System.exit(1);
+		}
+		
+		//Executing and wait the data generator
 		ProcessBuilder pb = new ProcessBuilder( "java", "-jar", jarName, secondsOfGeneration );
+		pb.redirectOutput( this.outputFile );
+		pb.redirectError( this.errorFile );
 		try {
 			this.dataGenerator = pb.start();
+			this.dataGenerator.waitFor();
 		} catch (IOException e) {
-			System.err.println("Cannot start: " + jarName );
-			System.exit(0);
+			System.err.println("Cannot execute: " + jarName + "\nTerminate execution" );
+			System.exit(2);
+		} catch (InterruptedException e) {
+			System.err.println("Cannot wait: " + jarName + "\nTerminate execution" );
+			System.exit(3);
 		}
-
-		//Reading the standard output and the standard error
-		this.brStd = new BufferedReader( new InputStreamReader( dataGenerator.getInputStream() ) );
-		//this.brErr = new BufferedReader( new InputStreamReader( dataGenerator.getErrorStream() ) );
-		this.isr = new InputStreamReader( dataGenerator.getErrorStream() );
+		
+		//Prepare reader for generated output
+		try {
+			this.brStd = new BufferedReader( new FileReader( outputFile ) );
+			this.brErr = new BufferedReader( new FileReader( errorFile ) );
+		} catch (FileNotFoundException e) {
+			System.err.println("Temporary files not found\nTerminate execution" );
+			System.exit(4);
+		}
 	}
 	
+	public String readOneErrLine() throws IOException{
+		return this.brErr.readLine();
+	}
+	
+	public String readOneStdLine() throws IOException{
+		return this.brStd.readLine();
+	}
+	
+//	/**
+//	 * Reader without buffered reader - Still has the bug
+//	 * @return
+//	 * @throws IOException
+//	 */
 //	public String readOneLine() throws IOException{
-//		return this.brErr.readLine();
+//		String myString = "";
+//		char[] cbuf = {'a'};
+//		while(cbuf[0] != '\n'){
+//			isr.read(cbuf);
+//			myString = myString + cbuf[0];
+//		}
+//		System.out.println(myString);
+//		return myString.trim();
 //	}
-	
-	/**
-	 * Reader without buffered reader - Still has the bug
-	 * @return
-	 * @throws IOException
-	 */
-	public String readOneLine() throws IOException{
-		String myString = "";
-		char[] cbuf = {'a'};
-		while(cbuf[0] != '\n'){
-			isr.read(cbuf);
-			myString = myString + cbuf[0];
-		}
-		System.out.println(myString);
-		return myString.trim();
-	}
 }
--- src/core/Start.java
+++ src/core/Start.java
@@ -9,35 +9,54 @@ public class Start {
 
 	/*
 	 * ###BUG
-	 * - With more than 10 houses the BufferedReader stops to read.
 	 */
 	public static void main(String[] args) {
 		String pathToGenerator = "/home/timmy/Documenti/Progetti_Eclipse/Middleware/OpenMPITest/src/data-generator.jar";
-		Integer secOfCollection = 3;
+		Integer secOfCollection = 1500;
 		long startTime = System.currentTimeMillis();
 		ArrayList<House> myHouses = new ArrayList<House>();
+		
+		//Here I generate data and prepare the reader.
 		Reader myReader = new Reader( secOfCollection, pathToGenerator );
 		long runningTime = System.currentTimeMillis();
-		String readStuff = "";
-		int i = 0;
-		while( true ){
-			try {
-				readStuff = myReader.readOneLine();
-				if( readStuff == null ){
-					break;
-				}
-				myHouses.add( new House( readStuff ) );
-				
-				i++;
-				System.out.println("house num " + i);
-				
-			} catch (IOException e) { System.err.println("Cannot read any line"); }
-		}
+		
+		//Generate the world
+		generateWorld(myReader, myHouses);
+		
+		
 		long creatingClassesTime = System.currentTimeMillis();
 		System.out.println("Creation finished");
 		System.out.println("Creation data time: "+ (runningTime-startTime) + " ms.");
 		System.out.println("Creation world time: "+ (creatingClassesTime-runningTime) + " ms.");
 		System.out.println("Total generation time: "+ (creatingClassesTime-startTime) + " ms.");
+	}
+
+	/**
+	 * Read data from stdErr and generates the world.
+	 * @param myReader	Where I take information.
+	 * @param myHouses	Where I collect the houses read.
+	 */
+	private static void generateWorld(Reader myReader, ArrayList<House> myHouses) {
+		String readStuff = "";
+		while( !finishedToRead( readStuff ) ){
+			try {
+				readStuff = myReader.readOneErrLine();
+				if( !finishedToRead( readStuff ) ){
+					myHouses.add( new House( readStuff ) );
+				}
+			} catch (IOException e) { 
+				System.err.println("Cannot read any line");
+			}
+		}
+	}
+
+	/**
+	 * Evaluates if the end of file is reached.
+	 * @param readStuff	What has been read.
+	 * @return	true -> There isn't nothing to read.
+	 */
+	private static boolean finishedToRead(String readStuff) {
+		return readStuff == null;
 	}
 
 }
